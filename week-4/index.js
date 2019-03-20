a15b091005a644d7ece82e4c83deae3cb4bc4bb9
sagacious-flag-2805 <avery.chung@wework.com>
2019-03-20 22:22:01 +0000

    Automatically backed up by Learn

--- week-4/index.js
+++ week-4/index.js
@@ -1,6 +1,32 @@
-function createAnElement {
-  let newDiv = document.createDocument('div')
+// <div>red</div>
+// <div>orange</div>
+
+function makeColorfulDiv(color) {
+  // 1. make a div
+let newDiv = document.createElement('div')
+// red = <div></div>
+// 2. <div>red</div>
+newDiv.innerHTML = color
+// 3. div's background color is red
+// <div style="background-color: red">
+newDiv.style.backgroundColor = color
+// 4. <div id="colors"> <div> red </div> </div>
+
+// 1. find #colors div
+const colors = document.getElementById('colors')
+// 2. Append the child to the #colors div
+colors.appendChild(newDiv)
 }
 
-function makeColorfulDiv(color)
-  //this function is big, in terms of all t
+
+makeColorfulDiv('red')
+makeColorfulDiv('orange')
+makeColorfulDiv('yellow')
+makeColorfulDiv('green')
+makeColorfulDiv('blue')
+makeColorfulDiv('indigo')
+makeColorfulDiv('purple')
+makeColorfulDiv('cadetblue')
+makeColorfulDiv('mistyrose')
+makeColorfulDiv('goldenrod')
+
